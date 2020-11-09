f6a087e8c7addfd54732aababc5316046e23c167
hieuviet1103 <hieuviet.1103@gmail.com>
2020-11-09 08:51:54 +0700

    Add project files.

---
+++ WebApplication3/Areas/DieuHanh/Controllers/LoginController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Areas.DieuHanh.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private readonly GroceryHomeEntities dB = new GroceryHomeEntities();
+        Models.LoginSatus loginStatus = new Models.LoginSatus();
+
+        // GET: DieuHanh/Login
+
+        //public ActionResult Index()
+        //{
+        //    if (!string.IsNullOrEmpty(Request["userName"]) && !string.IsNullOrEmpty(Request["passWord"]))
+        //    {
+        //        LoginViewModel model = new LoginViewModel();
+        //        model.UserName = Request["userName"];
+        //        model.Password = Request["passWord"];
+        //        return View("Login", model);
+        //    }
+        //    return View();
+        //}
+        public ActionResult Index(LoginViewModel model)
+        {
+            if (!string.IsNullOrEmpty(Request["userName"]) && !string.IsNullOrEmpty(Request["passWord"]))
+            {
+                model.UserName = Request["userName"];
+                model.Password = Request["passWord"];
+                return View(model);
+            }
+            return View("Login");
+        }
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        { 
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                if (dB.tbl_User.Any(d => d.User_Name == model.UserName))
+                {
+                    if (dB.tbl_User.Any(d => d.Password == model.Password))
+                    {
+                        Response.Cookies["userName"].Value = model.UserName;
+                        Response.Cookies["passWord"].Value = model.Password;
+                        return View("Index", model);
+                    }
+                    else
+                    {
+                        model.LoginError = "*Mật khẩu không chính xác!";
+                    }
+                }
+                else
+                {
+                    model.LoginError = "*Tên tài khoản không chính xác!";
+                }
+            }
+            catch
+            {
+                model.LoginError = "Lỗi không xác định!";
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            Response.Cookies["userName"].Value = null;
+            Response.Cookies["passWord"].Value = null;
+            return View("Login");
+        }
+    }
+}
---
+++ WebApplication3/Areas/DieuHanh/Models/LoginSatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Areas.DieuHanh.Models
+{
+    public class LoginSatus
+    {
+        public static bool IsloginAdmin { get; set; }
+        public string UserName = string.Empty;
+        public string PassWord = string.Empty;
+
+    }
+}
