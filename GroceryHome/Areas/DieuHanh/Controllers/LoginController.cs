dc9c673b4e52074ef1772fb657bac6651cc05e3a
hieuviet1103 <hieuviet.1103@gmail.com>
2020-11-13 14:14:20 +0700

    change all

---
+++ GroceryHome/Areas/DieuHanh/Controllers/LoginController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using GroceryHome.Areas.DieuHanh.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GroceryHome.Areas.DieuHanh.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private readonly GroceryHomeEntities dB = new GroceryHomeEntities();
+
+        // GET: DieuHanh/Login
+
+        //public ActionResult Index()
+        //{
+        //    if (!string.IsNullOrEmpty(Request["userName"]) && !string.IsNullOrEmpty(Request["passWord"]))
+        //    {
+        //        LoginViewModel model = new LoginViewModel();
+        //        model.UserName = Request["userName"];
+        //        model.Password = Request["passWord"];
+        //        return View("Login", model);
+        //    }
+        //    return View();
+        //}
+
+
+        public async Task<ActionResult> Index(LoginViewModel model)
+        {
+            if (!string.IsNullOrEmpty(Session["CurrentUser"].ToString()))
+            {
+                model.UserName = Session["CurrentUser"].ToString();
+                return View(model);
+            }
+            return View("Login");
+        }
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                if (dB.tbl_User.Any(d => d.User_Name == model.UserName))
+                {
+                    if (dB.tbl_User.Any(d => d.Password == model.Password))
+                    {
+                        Session["CurrentUser"] = model.UserName;
+                        LoginSatus.IsloginAdmin = true;
+                        LoginSatus.UserLogin = model.UserName;
+                        //return View("Index", model);
+                        return RedirectToAction("Index", "AdminHome");
+                    }
+                    else
+                    {
+                        model.LoginError = "*Mật khẩu không chính xác!";
+                    }
+                }
+                else
+                {
+                    model.LoginError = "*Tên tài khoản không chính xác!";
+                }
+            }
+            catch
+            {
+                model.LoginError = "Lỗi không xác định!";
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            Session["CurrentUser"] = null;
+            LoginSatus.IsloginAdmin = false;
+            return View("Login");
+        }
+    }
+}
---
+++ GroceryHome/Areas/DieuHanh/DieuHanhAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace GroceryHome.Areas.DieuHanh
+{
+    public class DieuHanhAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DieuHanh";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DieuHanh_default",
+                "DieuHanh/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
